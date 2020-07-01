fbfb8b59c272b6ec334767e008211c10dc3c19b9
BlackDrag0n <pmk.drag0n.1998@gmail.com>
2020-07-01 14:39:45 +0700

    add device_list

---
+++ device_list.py
@@ -0,0 +1,15 @@
+sw1 = {
+    "device_type":"cisco_ios",
+    "ip":"10.215.26.235",
+    "username":"admin",
+    "password":"123",
+    "secret":"321"
+    }
+sw2 = {
+    "device_type":"cisco_ios",
+    "ip":"10.215.26.234",
+    "username":"admin",
+    "password":"123",
+    "secret":"321"
+    }
+devices=[sw1,sw2]
